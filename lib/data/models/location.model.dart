f73dfe1fe52e6d3d8410a18b22fc81d68b107d2d
vinc <appleyardvincent@gmail.com>
2024-07-17 10:14:42 +0100

    location data model

---
+++ lib/data/models/location.model.dart
@@ -0,0 +1,8 @@
+import 'package:weather_app/domain/entities/location.dart';
+
+class LocationModel extends Location {
+  LocationModel({
+    required super.longitude,
+    required super.latitude,
+  });
+}
