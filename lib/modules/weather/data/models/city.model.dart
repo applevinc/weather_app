c35088b5f7e68c8a63c07bc4438907e35d567754
vinc <appleyardvincent@gmail.com>
2024-07-17 15:36:05 +0100

    city remote data source impl

--- lib/modules/weather/data/models/city.model.dart
+++ lib/modules/weather/data/models/city.model.dart
@@ -1,4 +1,6 @@
+import 'package:weather_app/modules/weather/data/models/location.model.dart';
 import 'package:weather_app/modules/weather/domain/entities/city.dart';
+import 'package:weather_app/utils/functions.dart';
 
 class CityModel extends City {
   const CityModel({
@@ -12,6 +14,17 @@ class CityModel extends City {
       id: entity.id,
       name: entity.name,
       location: entity.location,
+    );
+  }
+
+  factory CityModel.fromJson(Map<String, dynamic> json) {
+    return CityModel(
+      id: faker.guid.guid(),
+      name: json['city'],
+      location: LocationModel(
+        latitude: double.parse(json['lat']),
+        longitude: double.parse(json['lng']),
+      ),
     );
   }
 }
--- lib/modules/weather/data/sources/impls/city_remote_data_source.dart
+++ lib/modules/weather/data/sources/impls/city_remote_data_source.dart
@@ -1,17 +1,56 @@
+import 'package:dio/dio.dart';
 import 'package:weather_app/modules/weather/data/models/city.model.dart';
 import 'package:weather_app/modules/weather/data/sources/interfaces/i_city_data_source.dart';
 import 'package:weather_app/utils/models/failure.model.dart';
 
+abstract class _Urls {
+  static String cities =
+      'https://creditzikoyi-my.sharepoint.com/personal/oeminokanju_renmoney_com/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Foeminokanju%5Frenmoney%5Fcom%2FDocuments%2FMobile%20Assessment%2Fng%2Ejson&parent=%2Fpersonal%2Foeminokanju%5Frenmoney%5Fcom%2FDocuments%2FMobile%20Assessment&ga=1';
+}
+
 class CityRemoteDataSource implements ICityDataSource {
   @override
   Future<List<CityModel>> getAll() async {
-    // TODO: implement getAll
-    throw InternalFailure();
+    return await fetchDataAndHandleErrors(() async {
+      final response = await Dio().get(_Urls.cities);
+
+      if (response.statusCode == 200) {
+        final List collection = response.data;
+        return collection.map((e) => CityModel.fromJson(e)).toList();
+      } else {
+        throw Failure('Something went wrong while fetching cities');
+      }
+    });
   }
 
   @override
   Future<List<CityModel>> getFavoriteCities() async {
-    // TODO: implement getFavoriteCities
-    throw InternalFailure();
+    return await fetchDataAndHandleErrors(() async {
+      final response = await Dio().get(_Urls.cities);
+
+      if (response.statusCode == 200) {
+        final List collection = response.data;
+        final cities = collection.map((e) => CityModel.fromJson(e)).toList();
+        List<CityModel> filteredCities = [];
+
+        for (final city in cities) {
+          if (city.name.toLowerCase() == 'lagos') {
+            filteredCities.add(city);
+          }
+
+          if (city.name.toLowerCase() == 'ibadan') {
+            filteredCities.add(city);
+          }
+
+          if (city.name.toLowerCase() == 'abuja') {
+            filteredCities.add(city);
+          }
+        }
+
+        return filteredCities;
+      } else {
+        throw Failure('Something went wrong while fetching cities');
+      }
+    });
   }
 }
