034c7cea3902332872564921fac84535bf78844a
vinc <appleyardvincent@gmail.com>
2024-07-17 12:30:39 +0100

    add city local data sources to locators

--- lib/config/locators/mock.locator.dart
+++ lib/config/locators/mock.locator.dart
@@ -1,8 +1,10 @@
 import 'package:get_it/get_it.dart';
 import 'package:weather_app/config/app_config.dart';
 import 'package:weather_app/modules/weather/data/sources/fakes/fake_city_data_source.dart';
+import 'package:weather_app/modules/weather/data/sources/fakes/fake_city_local_data_source.dart';
 import 'package:weather_app/modules/weather/data/sources/fakes/fake_weather_data_source.dart';
 import 'package:weather_app/modules/weather/data/sources/interfaces/i_city_data_source.dart';
+import 'package:weather_app/modules/weather/data/sources/interfaces/i_city_local_data_source.dart';
 import 'package:weather_app/modules/weather/data/sources/interfaces/i_weather_data_source.dart';
 
 final GetIt mockLocator = GetIt.instance;
@@ -17,5 +19,9 @@ void initLocatorForEnv({required AppConfig appConfig}) async {
 
   mockLocator.registerLazySingleton<ICityDataSource>(
     () => FakeCityDataSource(),
+  );
+
+  mockLocator.registerLazySingleton<ICityLocalDataSource>(
+    () => FakeCityLocalDataSource(),
   );
 }
--- lib/config/locators/qa.locator.dart
+++ lib/config/locators/qa.locator.dart
@@ -1,8 +1,10 @@
 import 'package:get_it/get_it.dart';
 import 'package:weather_app/config/app_config.dart';
 import 'package:weather_app/modules/weather/data/sources/impls/city_data_source.dart';
+import 'package:weather_app/modules/weather/data/sources/impls/city_local_data_source.dart';
 import 'package:weather_app/modules/weather/data/sources/impls/weather_data_source.dart';
 import 'package:weather_app/modules/weather/data/sources/interfaces/i_city_data_source.dart';
+import 'package:weather_app/modules/weather/data/sources/interfaces/i_city_local_data_source.dart';
 import 'package:weather_app/modules/weather/data/sources/interfaces/i_weather_data_source.dart';
 
 final GetIt qaLocator = GetIt.instance;
@@ -17,5 +19,9 @@ void initLocatorForEnv({required AppConfig appConfig}) async {
 
   qaLocator.registerLazySingleton<ICityDataSource>(
     () => CityDataSource(),
+  );
+
+  qaLocator.registerLazySingleton<ICityLocalDataSource>(
+    () => CityLocalDataSource(),
   );
 }
