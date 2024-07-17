5ceb02d3fc9dc0f5767f16e019f46a7faf3b33d6
vinc <appleyardvincent@gmail.com>
2024-07-17 10:09:42 +0100

    city data model

---
+++ lib/data/models/city.model.dart
@@ -0,0 +1,9 @@
+import 'package:weather_app/domain/entities/city.dart';
+
+class CityModel extends City {
+  CityModel({
+    required super.id,
+    required super.name,
+    required super.location,
+  });
+}
