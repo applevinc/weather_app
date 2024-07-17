0593e5fa5175b7b19f49350893ca6b955681b680
vinc <appleyardvincent@gmail.com>
2024-07-17 10:17:05 +0100

    weather data model

---
+++ lib/data/models/weather.model.dart
@@ -0,0 +1,13 @@
+import 'package:weather_app/domain/entities/weather.dart';
+
+class WeatherModel extends Weather {
+  WeatherModel({
+    required super.condition,
+    required super.temperature,
+    required super.minTemperature,
+    required super.maxTemperature,
+    required super.city,
+    required super.type,
+    required super.date,
+  });
+}
